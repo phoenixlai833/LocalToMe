ed376e6cdcaa5c14980606a905532e98869204d2
Phoenix Lai <plai40@my.bcit.ca>
2022-10-10 19:22:05 -0700

    Phoenix-createfbpage_makesideinfobar_linkthem

---
+++ components/MapSlideUp/index.jsx
@@ -0,0 +1,34 @@
+
+import React, { useState, useEffect } from 'react';
+import * as foodbankData from "../../pages/data/foodbank.json";
+import styles from './MapSlideUp.module.css'
+import Link from 'next/link'
+
+export default function MapSlideUp({ props }) {
+    // console.log(props)
+
+    const foodBankComponent = foodbankData.map((fb) => (
+        <li key={fb.recordid} className={styles.card}>
+            <div className={styles.flexLayout}>
+                <div className={styles.fbImage}>
+                    image
+                </div>
+                <div className={styles.fbInfo}>
+                    <Link href={`/foodBankMap/x028IyjUQTdZsZ4gwftE`}>
+                        <a><h4 className={styles.noSpace}>{fb.fields.program_name}</h4></a>
+                    </Link>
+                    <p>{fb.fields.location_address}</p>
+                </div>
+            </div>
+        </li>
+    ))
+
+    return (
+        <div>
+            <div className={styles.background}>
+                <ul className={styles.noSpace}>{foodBankComponent}
+                </ul></div>
+            <button className={styles.pos}>SLIDE</button>
+        </div>
+    )
+}
