a6ea74680f32e06f9d4017fc6af0feab5ddaeab5
Phoenix Lai <plai40@my.bcit.ca>
2022-10-13 12:24:51 -0700

    Phoenix-fix-dependencies-change-to-yarn

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -10,7 +10,7 @@ ui.start('#firebaseui-auth-container', {
         //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID
+        // firebase.auth.GithubAuthProvider.PROVIDER_ID
     ],
     // Other config options...
 });
