acf1f7035aa0e8e01324ecfa75c38e0d19fb1e37
pho86 <pho86@my.bcit.ca>
2022-10-26 09:07:19 -0700

    fix props for buttons in storybook

--- components/TopBanner/TopBanner.stories.jsx
+++ components/TopBanner/TopBanner.stories.jsx
@@ -4,13 +4,13 @@ import TopBanner from ".";
 export default {
    title: 'TopBanner',
    component: TopBanner,
- };
+};
 
- const Default = (args) => <TopBanner {...args} />;
+const Template = (args) => <TopBanner {...args} />;
 
-export const Base = Default.bind({});
+export const Default = Template.bind({});
 
 Default.args = {
-   text:"1234 BCIT Street",
-   back:true
+   text: "1234 BCIT Street",
+   back: true
 };
