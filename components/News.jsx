e10635e8286c982562fe9fa106e5efc8a3f9482b
Phoenix Lai <plai40@my.bcit.ca>
2022-10-26 23:47:02 -0700

    phoenix-update

--- components/News.jsx
+++
@@ -1,9 +0,0 @@
-export default function News({ newsId, newsTitle, newsImage }) {
-
-  return (
-    <div>
-      <img width="100" height="100" src={newsImage} alt={newsTitle} />
-      <p>{newsTitle}</p>
-    </div>
-  );
-}
--- components/PostCreationButtons/postcreationbuttons.stories.js
+++
@@ -1,9 +0,0 @@
-import CreationBttn from "."
-
-export default {
-    title: "Creation Button",
-    component: CreationBttn
-}
-
-export const Default = () => <CreationBttn/>
-export const Error = () => <CreationBttn />
