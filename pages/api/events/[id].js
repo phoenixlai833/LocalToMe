1a350188db1901c6cf8bc2d0479790a9674ae2cf
Hoang Cuong Nguyen <cuong@Cuongs-Air.hitronhub.home>
2022-11-02 09:50:17 -0700

    sync db with algolia, fix community page

--- pages/api/events/[id].js
+++ pages/api/events/[id].js
@@ -1,19 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as db from "../../../server/database";
-const algoliasearch = require('algoliasearch');
-const client = algoliasearch(process.env.NEXT_PUBLIC_ALGOLIA_CLIENT_ID, process.env.NEXT_PUBLIC_ALGOLIA_API_KEY);
+import algoliasearch from "algoliasearch";
+const client = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_CLIENT_ID,
+  process.env.NEXT_PUBLIC_ALGOLIA_API_KEY_ADMIN
+);
+
+const index = client.initIndex("prod_EVENTS");
 
 export default async function handler(req, res) {
-    
   if (req.method === "GET") {
     // Handle GET requests
-        const event = await db.getEvents();
-        // res.status(200).json(events);
+    const event = await db.getEvent(+req.params.id);
+    res.status(200).json(event);
   } else if (req.method === "POST") {
     // Handle POST requests
   } else if (req.method === "PUT") {
     // Handle PUT requests
   } else if (req.method === "DELETE") {
     // Handle DELETE requests
+    await db.deleteEvent(+req.params.id);
+    index.deleteObject(+req.params.id)
+    res.sendStatus(200);
   }
 }
