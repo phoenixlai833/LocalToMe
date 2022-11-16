05230b92447c30f6ff34f7fcb46ba26def104669
yoyochen68 <90425062+yoyochen68@users.noreply.github.com>
2022-11-16 01:09:53 -0800

    update the empty favorite page

--- pages/favourites.jsx
+++ pages/favourites.jsx
@@ -21,7 +21,7 @@ margin-bottom: 5%;
 
 const Text = styled.h1`
 
-margin: 0 auto;
+margin: auto;
 color: #108928;
 justify-content: center;
 align-self: center;
@@ -30,57 +30,69 @@ align-self: center;
 
 
 export default function Favorites({ user }) {
-    const [userFavorites, setUserFavorites] = useState(user.favorite);
 
-    const handleUnfavorite = (type, itemId) => {
-        postFavorite(true, type, user.id, itemId).then(() => {
-            // const newUserFavorites = userFavorites;
-            const newUserFavorites = { ...userFavorites };
-            newUserFavorites[type] = userFavorites[type].filter((item) => item.id !== itemId)
-            console.log('here', userFavorites, '||', newUserFavorites)
-            setUserFavorites(newUserFavorites);
-        })
+    if (user.favorite.event.length > 0 || user.favorite.location.length > 0) {
+        const [userFavorites, setUserFavorites] = useState(user.favorite);
+
+        const handleUnfavorite = (type, itemId) => {
+            postFavorite(true, type, user.id, itemId).then(() => {
+                // const newUserFavorites = userFavorites;
+                const newUserFavorites = { ...userFavorites };
+                newUserFavorites[type] = userFavorites[type].filter((item) => item.id !== itemId)
+                console.log('here', userFavorites, '||', newUserFavorites)
+                setUserFavorites(newUserFavorites);
+            })
+        }
+        const eventList = userFavorites.event.map((singleEvent) => (
+            <FavCard key={singleEvent.id} id={singleEvent.id} type="event" href={`/events/${singleEvent.id}`} img={singleEvent.eventImage} txt="Event" title={singleEvent.eventName} location={singleEvent.eventLocation} onUnfavorite={handleUnfavorite} backgroundColor="#FFB800" txtColor="#000000" />
+        ))
+
+        const locationList = userFavorites.location.map((singleLocation) => (
+            <FavCard key={singleLocation.id} id={singleLocation.id} type="location" href={`/foodBank/${singleLocation.id}`} img={singleLocation.foodBank_Image} txt="Food Bank" title={singleLocation.program_name} location={singleLocation.location_address} onUnfavorite={handleUnfavorite} backgroundColor="#108928" txtColor="#FFFFFF" />
+        ))
+
+        const allList = locationList.concat(eventList)
+
+
+
+        const [tab, setTab] = useState(0);
+
+        const tabContents = {
+            0: { component: allList },
+            1: { component: locationList },
+            2: { component: eventList },
+        };
+        const tabList = ["All", "Locations", "Events"];
+        const handleChangeTab = (tabId) => {
+            setTab(tabId)
+        };
+
+
+        return (
+            <>
+                <Search />
+                <Tabs tabId={tab} onChangeTab={handleChangeTab} tabList={tabList} />
+                <FavorPage>
+
+                    {tabContents[tab].component || <Text>There are no favorites yet</Text>}
+                    <NavBar value={3} />
+                </FavorPage>
+
+            </>
+        )
+    } else {
+        return (
+            <>
+
+                <FavorPage>
+                    <Text>There are no favorites yet</Text>
+                    <NavBar value={3} />
+                </FavorPage>
+            </>
+        )
+
     }
-    const eventList = userFavorites.event.map((singleEvent) => (
-        <FavCard key={singleEvent.id} id={singleEvent.id} type="event" href={`/events/${singleEvent.id}`} img={singleEvent.eventImage} txt="Event" title={singleEvent.eventName} location={singleEvent.eventLocation} onUnfavorite={handleUnfavorite} backgroundColor="#FFB800" txtColor="#000000" />
-    ))
-
-    const locationList = userFavorites.location.map((singleLocation) => (
-        <FavCard key={singleLocation.id} id={singleLocation.id} type="location" href={`/foodBank/${singleLocation.id}`} img={singleLocation.foodBank_Image} txt="Food Bank" title={singleLocation.program_name} location={singleLocation.location_address} onUnfavorite={handleUnfavorite} backgroundColor="#108928" txtColor="#FFFFFF" />
-    ))
-
-    const allList = locationList.concat(eventList)
-
-
-
-    const [tab, setTab] = useState(0);
-
-    const tabContents = {
-        0: { component: allList },
-        1: { component: locationList },
-        2: { component: eventList },
-    };
-    const tabList = ["All", "Locations", "Events"];
-    const handleChangeTab = (tabId) => {
-        setTab(tabId)
-    };
-
-
-    console.log("uuuuu", user.favorite.event);
-    return (
-        <>
-            <Search />
-            <Tabs tabId={tab} onChangeTab={handleChangeTab} tabList={tabList} />
-            <FavorPage>
-
-                {tabContents[tab].component || <Text>There are no favorites yet</Text>}
-                <NavBar value={3} />
-            </FavorPage>
-
-        </>
-    )
 }
-
 export async function getServerSideProps(context) {
     const session = await unstable_getServerSession(context.req, context.res, authOptions)
 
