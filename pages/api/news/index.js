72f352cc7a47fab3eacb7f46874bfcd4c31f6ac3
yoyochen68 <90425062+yoyochen68@users.noreply.github.com>
2022-11-21 18:56:44 -0800

    update the nextauth path

--- pages/api/news/index.js
+++ pages/api/news/index.js
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as db from "../../../server/database";
 import { unstable_getServerSession } from "next-auth/next";
-import { authOptions } from "./auth/[...nextauth]";
+import { authOptions } from "..[...nextauth]";
+
+
 
 export default async function handler(req, res) {
   const session = await unstable_getServerSession(req, res, authOptions);
