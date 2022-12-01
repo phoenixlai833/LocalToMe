ae7954131835cd5f4165ce570469192838162686
Hoang Cuong Nguyen <hcuong.ng.ca@gmail.com>
2022-11-30 18:08:35 -0800

    fix landing stuffs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/landing/splash',
+        permanent: false,
+      },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
